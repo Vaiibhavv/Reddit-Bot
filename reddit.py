357a559930bb8d9b01cbec0e9a2f482c7e39283d
vaibhav <vaibhavsaner@gmail.com>
2023-09-03 15:35:58 +0530

    hidden

--- reddit.py
+++ reddit.py
@@ -3,11 +3,11 @@ import praw
 import time
 
 reddit = praw.Reddit(
-    client_id="EDdwWh0D6SAt0YeDkhO9rg",
-    client_secret="15PhB9t0Z8bKRUyoKx90zyhiNvrgXA",
+    client_id="enter your reddit id",
+    client_secret="secret key",
     user_agent="<console:redpyapp:1.0>",
-    username="vaibhmaster",
-    password="reddit@12"
+    username="user name",
+    password="enter your password"
 )
 sad_quote= ["All our dreams can come true, if we have the courage to pursue them. —Walt Disney","The secret of getting ahead is getting started. —Mark Twain","I’ve missed more than 9,000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life, and that is why I succeed. —Michael Jordan","Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve. —Mary Kay Ash" ,"The best time to plant a tree was 20 years ago. The second best time is now. ―Chinese Proverb"]
 
